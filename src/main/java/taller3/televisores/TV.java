44d0a18bc9bf30a92f22839140f66302b6612a92
nsalinas350 <nsalinas@unal.edu.co>
2021-11-02 08:43:56 -0500

    Create classes

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,93 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	private boolean estado;
+	private int volumen = 1;
+	private Control control;
+	private static int numTV;
+	
+	public TV(Marca marca , boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+	public void setControl(Control control) {
+		this.control = control;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}	
+	
+	public static int getnumTV() {
+		return numTV;
+	}
+	
+	public void turnOn() {
+		this.estado = true;
+	}
+	public void turnOff() {
+		this.estado = false;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void canalUp() {
+		if (this.getEstado() == false) {
+			return;}
+		if (canal<120) {
+			canal++;}
+	}
+	public void canalDown() {
+		if (this.getEstado() == false) {
+			return;}
+		if (canal>1) {
+			canal--;}
+	}
+	
+	public void volumenUp() {
+		if (this.getEstado() == false) {
+			return;}
+		if (volumen<7) {
+			volumen++;}
+	}
+	public void volumenDown() {
+		if (this.getEstado() == false) {
+			return;}
+		if (volumen>0) {
+			volumen--;}
+	}
+	
+}
